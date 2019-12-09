52249e1dca07aefa5594ebf710e809178ea4be28
holy-guacajolie <57738210+holy-guacajolie@users.noreply.github.com>
2019-12-09 12:09:31 -0500

    added isLast thingie

--- Question.js
+++ Question.js
@@ -1,9 +1,10 @@
 class Question {
-    constructor(q, a1, a2, a3, a4, ca) {
+    constructor(q, a1, a2, a3, a4, ca, isLast) {
         this.quest = q; //question string
         this.answers = []; //array of answer strings
         this.answers = [a1, a2, a3, a4];
         this.correctInd = ca; //index of correct answer
+        this.last = isLast;
     }
     getQuestion() {
         return this.quest;
@@ -20,5 +21,8 @@ class Question {
     isCorrect(index) {
         if (index == this.correctInd) {return true;}
         else {return false;}
+    }
+    isLast() {
+        return this.isLast;
     }
 }
